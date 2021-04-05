cd15a6b119703fa6a282d631fc2833b7edf3e67e
adi forkasiewicz <adiforka@gmail.com>
2021-04-05 16:45:44 +0200

    switching to consistent snake case in file names

--- src/exercises/assorted/recursion_practice.ts
+++ src/exercises/assorted/recursion_practice.ts
@@ -1,20 +1,20 @@
 let categories = [
-  { id: 'media', parent: null },
-  { id: 'audio', parent: 'media' },
-  { id: 'book', parent: 'media' },
-  { id: 'video', parent: 'media' },
-  { id: 'metal', parent: 'audio' },
-  { id: 'hip-hop', parent: 'audio' },
-  { id: 'jazz', parent: 'audio' },
-  { id: 'horror', parent: 'video' },
-  { id: 'comedy', parent: 'video' },
-  { id: 'action', parent: 'video' },
-  { id: 'non-fiction', parent: 'book' },
-  { id: 'romance', parent: 'book' },
-  { id: 'biography', parent: 'book' }
+  { id: "media", parent: null },
+  { id: "audio", parent: "media" },
+  { id: "book", parent: "media" },
+  { id: "video", parent: "media" },
+  { id: "metal", parent: "audio" },
+  { id: "hip-hop", parent: "audio" },
+  { id: "jazz", parent: "audio" },
+  { id: "horror", parent: "video" },
+  { id: "comedy", parent: "video" },
+  { id: "action", parent: "video" },
+  { id: "non-fiction", parent: "book" },
+  { id: "romance", parent: "book" },
+  { id: "biography", parent: "book" },
 ]
 
-let makeTree = (categories, parent) => {
+const makeTree = (categories, parent) => {
   let node = {}
   const sentinel = null
   categories
